cafb7b930e9c7c33b2aa5a73e078df9ecede42f9
Emanuel Borges <ebbmango@tuta.io>
2023-03-14 20:50:26 +0100

    playing around with asset compilation configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Trying to precompile assets by default
+  config.assets.debug = false
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
